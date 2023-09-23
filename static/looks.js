a91f9911c578a0d26a22f0d977e5f2832defa53c
n0rbed <yassinahmad789@gmail.com>
2023-09-23 12:45:30 -0400

    nav bar styling

--- static/looks.js
+++ static/looks.js
@@ -1,6 +1,7 @@
 window.addEventListener('load', () => {
     let nav_container = document.getElementById('nav_container');
-    nav_container.classList.add("fixed", 'top-0', 'w-screen', 'h-16', 'm-0', 'flex', 'flex-row', 'bg-gray-900', "text-white", "shadow-lg", "justify-around");
+    nav_container.classList.add("fixed", 'top-0', 'w-screen', 'h-20', 'm-0',
+        'flex', 'flex-row', 'bg-gray-900', "text-white", "shadow-lg", "justify-between");
 
     let lis = document.getElementsByClassName("li");
     for (let i = 0; i < lis.length; i++){
@@ -15,18 +16,27 @@ window.addEventListener('load', () => {
         window.location.assign('/signup');
     }
 
-    let nav_buttons = document.getElementsByClassName("nav_buttons")
-    for (let i = 0; i < nav_buttons.length; i++){
-        if(nav_buttons[i].innerHTML == "Login"){
-            nav_buttons[i].onclick = redirect_login
-        } else {
-            nav_buttons[i].onclick = redirect_signup
-        }
-        nav_buttons[i].classList.add("transition", "ease-in-out", "duration", "bg-white", 
-            "hover:bg-rose-200", "text-gray-800", "hover:text-gray-1000", 
-            "font-semibold", "py-2", "px-4", "border", "border-gray-400",
-            "rounded", "shadow")
-    }
+    let signup_button = document.getElementById('signup_button');
+    signup_button.classList.add("text-white", "bg-blue-700", "hover:bg-blue-800", 
+        "focus:outline-none", "focus:ring-4", "focus:ring-blue-300", 
+        "font-medium", "rounded-full", "text-sm", "px-5", "py-2.5", 
+        "text-center", "mr-2", "mb-2", "dark:bg-blue-600", "dark:hover:bg-blue-700", 
+        "dark:focus:ring-blue-800", "transition", "ease-in", "duration-75");
 
+    let login_button = document.getElementById('login_button');
+    login_button.classList.add("py-2.5", "px-5", "mr-2", "mb-2", "text-sm",
+        "font-medium", "text-gray-900", "focus:outline-none", "bg-white",
+        "rounded-full", "border", "border-gray-200", "hover:bg-gray-100",
+        "hover:text-blue-700", "focus:z-10", "focus:ring-4", "focus:ring-gray-200",
+        "dark:focus:ring-gray-700", "dark:bg-gray-800", "dark:text-gray-400",
+        "dark:border-gray-600", "dark:hover:text-white", "dark:hover:bg-gray-700",
+        "transition", "ease-in", "duration-75");
+
+    signup_button.onclick = () => { location.href = '/signup' };
+    login_button.onclick = () => { location.href = '/login' };
+
+    let logo = document.getElementById('logo')
+    logo.classList.add("transition-all", "ease-in-out", "duration-100", "mt-6",
+        "font-semibold", "text-xl", "ml-6", "hover:text-2xl", "hover:text-sky-200",
+        "hover:mt-4.5", "hover:ml-4.5")
 })
-
--- templates/tailwind.config.js
+++ templates/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    purge: { enabled: true, content: ["./*.{html}", "../static/*.js"]},
+    purge: { enabled: true, content: ["./*.html", "../static/*.js"]},
     theme: {
     extend: {},
     },
