db161d41598987ed1af8f2664db8252d04b498bf
n0rbed <yassinahmad789@gmail.com>
2023-09-23 17:03:03 -0400

    Fixed nav bar element locations and added another li with the correct positioning using flexbox. Flexbox is actually quite simple but i need to look at it a little deeper

--- static/looks.js
+++ static/looks.js
@@ -1,4 +1,18 @@
 window.addEventListener('load', () => {
+    let dark_mode = document.getElementById('dark_mode');
+    dark_mode.classList.add('h-full')
+    let html_tag = document.getElementsByTagName('html')[0];
+    dark_mode.onclick = () => {
+        if (html_tag.classList.contains('dark')){
+            html_tag.classList.remove('dark')
+        } else {
+            html_tag.classList.add('dark');
+        }
+    }
+    let dark_mode_svg = document.getElementById('dark_mode_svg')
+    dark_mode_svg.classList.add('h-full', 'w-10', 'mt-2')
+
+    document.body.classList.add('bg-antiwhite-100', 'dark:bg-antiwhite-300')
     let nav_container = document.getElementById('nav_container');
     nav_container.classList.add("fixed", 'top-0', 'w-screen', 'h-20', 'm-0',
         'flex', 'flex-row', 'bg-gray-900', "text-white", "shadow-lg", "justify-between");
@@ -7,24 +21,16 @@ window.addEventListener('load', () => {
     for (let i = 0; i < lis.length; i++){
         lis[i].classList.add('inline', 'p-3');
     }
-    
-    function redirect_login(){
-        window.location.assign('/login');
-    }
-
-    function redirect_signup(){
-        window.location.assign('/signup');
-    }
 
     let signup_button = document.getElementById('signup_button');
-    signup_button.classList.add("text-white", "bg-blue-700", "hover:bg-blue-800", 
-        "focus:outline-none", "focus:ring-4", "focus:ring-blue-300", 
+    signup_button.classList.add("text-white", "bg-chineseviolet-600", "hover:bg-chineseviolet-700", 
+        "focus:outline-none", "focus:ring-4", "focus:ring-chineseviolet-300", 
         "font-medium", "rounded-full", "text-sm", "px-5", "py-2.5", 
-        "text-center", "mr-2", "mb-2", "dark:bg-blue-600", "dark:hover:bg-blue-700", 
-        "dark:focus:ring-blue-800", "transition", "ease-in", "duration-75");
+        "text-center", "dark:bg-chineseviolet-700", "dark:hover:bg-chineseviolet-800", 
+        "dark:focus:ring-chineseviolet-800", "transition", "ease-in", "duration-75");
 
     let login_button = document.getElementById('login_button');
-    login_button.classList.add("py-2.5", "px-5", "mr-2", "mb-2", "text-sm",
+    login_button.classList.add("py-2.5", "px-5", "text-sm",
         "font-medium", "text-gray-900", "focus:outline-none", "bg-white",
         "rounded-full", "border", "border-gray-200", "hover:bg-gray-100",
         "hover:text-blue-700", "focus:z-10", "focus:ring-4", "focus:ring-gray-200",
@@ -37,6 +43,10 @@ window.addEventListener('load', () => {
 
     let logo = document.getElementById('logo')
     logo.classList.add("transition-all", "ease-in-out", "duration-100", "mt-6",
-        "font-semibold", "text-xl", "ml-6", "hover:text-2xl", "hover:text-sky-200",
+        "font-semibold", "text-xl", "ml-6", "hover:text-2xl", "hover:text-citrine",
         "hover:mt-4.5", "hover:ml-4.5")
+
+    let padlock = document.getElementById('padlock')
+    padlock.classList.add('mt-20', 'w-5/12')
+
 })
--- templates/tailwind.config.js
+++ templates/tailwind.config.js
@@ -1,8 +1,33 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     purge: { enabled: true, content: ["./*.html", "../static/*.js"]},
+    darkMode: 'class',
     theme: {
-    extend: {},
+    extend: {
+            colors: {
+                'antiwhite': {
+                    100: '#E7ECEF',
+                    200: '#CACDD1',
+                    300: '#ADAEB4',
+                    400: '#909096',
+                    500: '#747378',
+                },
+
+                'burgundy': '#74121D',
+                'citrine': '#EAD94C',
+                'chineseviolet': {
+                    100: '#CED1D5',
+                    200: '#BABCC4',
+                    300: '#A6A7B3',
+                    400: '#857F90',
+                    500: '#766C7F',
+                    600: '#6B5E6F',
+                    700: '#5F5160',
+                    800: '#50434E',
+                    900: '#40353C'
+                },
+            },
+        },
     },
     plugins: [],
 }
