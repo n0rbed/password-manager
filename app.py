b28f9699eaaf2e830406d53d066ebc9e18a7c145
n0rbed <yassinahmad789@gmail.com>
2023-09-23 20:29:20 -0400

    laid some more ground work for the sign up route, now i need to connect to data base using psql

--- app.py
+++ app.py
@@ -10,7 +10,22 @@ app = Flask(__name__)
 def home_page():
     return render_template('index.html')
 
+@app.route('/signup', methods=['POST', 'GET'])
+def signup():
+    if request.method == 'GET':
+        return render_template('signup.html')
 
-if __name__ == '__main__':
-    app.run(debug=True, host='localhost', port=7000)
+    email = request.form.get('email')
+    password = request.form.get('password')
+    confirmation = request.form.get('confirmation')
+
+    if not (email and password and confirmation):
+        return render_template('error.html', error='empty input field(s), error number 404')
+    if password != confirmation:
+        return render_template('error.html', error='password and confirmation do not match, error number 2')
+    # connect to data base and check if user already exists
+    # if not, hash password and then add it to database
+
+
+
 
