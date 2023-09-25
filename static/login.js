24e582c579334792ef004a01926330c63db61d05
n0rbed <yassinahmad789@gmail.com>
2023-09-25 18:49:06 -0400

    fixed login flex

--- static/login.js
+++ static/login.js
@@ -2,13 +2,15 @@ let container = document.getElementById('main_container')
 container.classList.add('flex', 'mt-40', 'items-center','justify-center', 'mx-auto',
                         'outline-dashed', 'outline-2', 'w-80', 'rounded-lg')
 
-document.getElementById('form').classList.add('flex-col', 'items-center')
+document.getElementById('form').classList.add('flex', 'flex-col', 'items-center')
 
 let title = document.getElementById('title')
-title.classList.add('pl-28', 'w-5', 'font-semibold')
+title.classList.add('w-14', 'font-semibold', 'mb-4', 'mt-2')
 
 let submit_button = document.getElementById('submit_login')
 submit_button.classList.add('bg-chineseviolet-900', 'rounded-lg', 'text-antiwhite-200',
                             'w-20', 'h-8')
+
+let inputs = document.getElementsByTagName('input')
 
 
